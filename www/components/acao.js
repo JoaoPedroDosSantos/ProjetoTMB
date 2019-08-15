84aa5c1e3d0712be8e0ef65807d21826e5b77a0e
JoaoPedroDosSantos <joaopedro-ken@hotmail.com>
2019-08-16 01:44:00 +0900

    terminado mas as função n ta funcionando

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,27 @@
-// This is a JavaScript file
+$(document).on("click","#calcular",function(){
 
+var resultado;
+var peso;
+var altura;
+var idade;
+var sexo;
+
+
+peso = $("#peso").val();
+altura = $("#altura").val();
+idade = $("#idade").val();
+sexo = $("#sexo").val();
+
+      if(sexo == 'masculino'){
+       $(resultado).val(66+(13.7*peso)+(5.0 * altura)(6.8*idade));
+       $("#visor").val(resultado);
+  }
+    if(sexo=='feminino'){
+       $(resultado).val(665 + (9.6 * peso) + (1.8*altura) - (4.7*idade));
+       $("#visor").val(resultado);
+  }
+
+
+
+
+}); 
